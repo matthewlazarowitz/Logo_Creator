c7766ccd68d25d8ee3aed6579e453e5b3de87f44
Laz <nyys811@aol.com>
2023-07-12 23:10:52 -0400

    created function for inquirer to prompt users for input on logo design

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const inquirer = require('inquirer');
 const { Circle, Triangle, Square } = require('./lib/shapes');
 const fs = require('fs');
+
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter the text for the logo (up to three characters):',
+        validate: function (value) {
+          if (value.length > 3) {
+            return 'Please enter up to three characters.';
+          }
+          return true;
+        },
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hexadecimal number):',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Select a shape:',
+        choices: ['Circle', 'Triangle', 'Square'],
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hexadecimal number):',
+      },
+    ]);
+  }
